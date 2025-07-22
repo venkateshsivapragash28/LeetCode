802a4b2b838a48e2aacf8de178f87f081a3eec5f
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-07-22 20:22:28 +0530

    Time: 3410 ms (5%), Space: 14.4 MB (5.1%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,19 +1,35 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        max_length = 0
+        # max_length = 0
+        # for i in range(len(s)):
+        #     count = 1
+        #     max_length = max(max_length, count)
+        #     for j in range(i+1, len(s)):
+        #         if s[j] not in s[i:j]:
+        #             count = count + 1
+        #             max_length = max(max_length, count)
+        #         else:
+        #             max_length = max(max_length, count)
+        #             break
+        # return max_length
+
+
+#22-07-2025, second attempt
+
+        string = ''
+        max_count = 0
+
         for i in range(len(s)):
+            string = ''
             count = 1
-            max_length = max(max_length, count)
+            string += s[i]
             for j in range(i+1, len(s)):
-                if s[j] not in s[i:j]:
-                    count = count + 1
-                    max_length = max(max_length, count)
+                if s[j] not in string:
+                    count+=1
+                    string+=s[j]
+                    max_count = max(max_count, count)
                 else:
-                    max_length = max(max_length, count)
                     break
-        return max_length
-
-
-
-                
-    
+            max_count = max(max_count, count)
+        return max_count                
+            
