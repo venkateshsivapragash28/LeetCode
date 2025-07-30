a6c935b636c96a7fb8174450a669047e1a02214e
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-07-30 21:59:05 +0530

    Time: 215 ms (71.77%), Space: 22 MB (72.21%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -6,8 +6,8 @@ class Solution(object):
             nums.sort() 
             diff = 0
             max_diff = 0
-            for i in range(1, len(nums)):
-                diff = nums[i] - nums[i - 1]
+            for i in range( len(nums)-1):
+                diff = nums[i+1] - nums[i]
                 if diff > max_diff:
                     max_diff = diff
             return max_diff
