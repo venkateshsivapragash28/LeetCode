d046c8920fc3944c726bf92e3134cf6315822707
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-07-31 22:08:54 +0530

    Time: 5040 ms (26.65%), Space: 12.4 MB (89.09%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def beautySum(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        total = 0
+        for i in range(len(s)):
+            temp = [0]*26
+            for j in range(i, len(s)):
+                index = ord(s[j]) - ord('a')
+                temp[index] = temp[index] + 1
+                maxx = max(temp)
+                minn = float('inf')
+                for x in temp:
+                    if x!= 0:
+                        minn = min(minn, x)
+                total += (maxx - minn)
+
+        return total
+
+
+        # 28 - 07 - 2025 ------------------------------------------------
+        # total = 0
+        # for i in range(len(s)):
+        #     for j in range(i, len(s)):
+        #         x = s[i:j+1]
+        #         if len(x) >= 3:
+        #             count = Counter(x)
+        #             most = max(count.values())
+        #             least = min(count.values())
+        #             total = total + (most - least)
+
+        # return total
+        
+        # ----------------------------------------------------------------
+
