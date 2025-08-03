2098ef1ec1be5c982adb67186ead3bfef4efd634
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-08-03 20:20:43 +0530

    Time: 0 ms (100%), Space: 12.4 MB (80.46%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -3,7 +3,6 @@ class Solution(object):
         i = 0
         while 2**i<=n:
             if 2**i == n:
-                i = i+1
                 return True
             else:
                 i = i+1
