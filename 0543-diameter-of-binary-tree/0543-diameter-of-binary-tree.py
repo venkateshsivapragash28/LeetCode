a16c3e87984822dd690cbf8eafb56353a87fd317
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-08-20 15:04:09 +0530

    Time: 7 ms (45.64%), Space: 20.9 MB (49.27%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -0,0 +1,59 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        #20 - 08 - 2025
+        # def height(node):
+
+        #     if not node:
+        #         return 0
+
+        #     else:
+        #         left_height = height(node.left)
+        #         right_height = height(node.right)
+
+        #         return 1 + max(left_height, right_height)
+
+        # dia = 0
+
+        # def diameter(node):
+        #     nonlocal dia
+        #     if not node:
+        #         return 
+
+        #     left_height = height(node.left) 
+        #     right_height = height(node.right)
+        #     dia = max(dia, left_height + right_height)
+
+        #     diameter(node.left)
+        #     diameter(node.right)
+        #     return dia
+
+        # return diameter(root) 
+
+        # 20 - 08 - 2025
+
+        dia = 0
+
+        def diameter(node):
+            nonlocal dia
+
+            if not node:
+                return 0
+
+            else:
+                left_height = diameter(node.left)
+                right_height = diameter(node.right)
+
+                dia = max(dia, left_height + right_height)
+
+                return 1 + max(left_height, right_height)
+
+        diameter(root)
+        return dia
