0aa8bd03d0ce94ab6685801552ee6d910db8dab4
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-08-28 08:28:54 +0530

    Time: 0 ms (100%), Space: 19.6 MB (76.82%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,19 +1,6 @@
-class Solution(object):
-    def singleNumber(self, arr):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        count = {}
-        for i in arr:
-            if i in count:
-                count[i] += 1
-            else:
-                count[i] = 1
-
-
-        for i in arr:
-            if count[i] == 1:
-                return i
-
-    
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
