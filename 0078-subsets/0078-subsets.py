9816590834f402b7c203241fea1a9156c11f3d84
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-08-28 19:56:34 +0530

    Time: 0 ms (100%), Space: 18.1 MB (21.64%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,37 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        arr = []
-        for i in range(0, (2 ** len(nums))):
-            x = bin(i)[2:].zfill(len(nums))
-            z = []
-            for j in range(len(str(x))):
-                if x[j] == '1':
-                    z.append(nums[j])
-            arr.append(z)
 
-        return arr
+        # Using Bit Manipulation !!!
+        #----------------------------- !!!!
+        # arr = []
+        # for i in range(0, (2 ** len(nums))):
+        #     x = bin(i)[2:].zfill(len(nums))
+        #     z = []
+        #     for j in range(len(str(x))):
+        #         if x[j] == '1':
+        #             z.append(nums[j])
+        #     arr.append(z)
+
+        # return arr
+        #----------------------------- !!!!
+
+        # Using recursion
+        res = []
+        n = len(nums)
+        def f(index, li):
+
+            if index >= n:
+                res.append(li[:])
+                return
+            
+            li.append(nums[index])
+
+            f(index + 1, li)
+
+            li.pop()
+
+            f(index + 1, li)
+
+        f(0, [])
+
+        return res
