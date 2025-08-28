618c9516ba6798d41bfe417276ee8860866be1b2
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-08-28 09:33:52 +0530

    Time: 3 ms (60.78%), Space: 18.8 MB (50.98%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,12 +1,10 @@
-class Solution(object):
-    def missingNumber(self, arr):
-        
-        maximum = max(arr)
-        actual_sum = 0
-        arr_sum = 0
-        for i in range(len(arr)+1):
-            actual_sum = actual_sum + i
-        for i in range(len(arr)):
-            arr_sum = arr_sum + arr[i]
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        xor_all = 0
+        xor_missing = 0
+        for i in nums:
+            xor_all ^= i
+        for i in range(len(nums)):
+            xor_missing ^= i
 
-        return abs(actual_sum - arr_sum)
+        return xor_all ^ xor_missing ^ len(nums)
