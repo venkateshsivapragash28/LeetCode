711ae97b299e4c0b71402ad1dd6a17c9f63b82ff
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-09-01 15:30:28 +0530

    Time: 31 ms (29.72%), Space: 12.4 MB (99.83%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -14,22 +14,59 @@ class Solution(object):
         # return max_length
 
 
-#22-07-2025, second attempt
+#22-07-2025, second attempt, passed completed
 
-        string = ''
-        max_count = 0
+        # string = ''
+        # max_count = 0
 
-        for i in range(len(s)):
-            string = ''
-            count = 1
-            string += s[i]
-            for j in range(i+1, len(s)):
-                if s[j] not in string:
-                    count+=1
-                    string+=s[j]
-                    max_count = max(max_count, count)
-                else:
-                    break
-            max_count = max(max_count, count)
-        return max_count                
+        # for i in range(len(s)):
+        #     string = ''
+        #     count = 1
+        #     string += s[i]
+        #     for j in range(i+1, len(s)):
+        #         if s[j] not in string:
+        #             count+=1
+        #             string+=s[j]
+        #             max_count = max(max_count, count)
+        #         else:
+        #             break
+        #     max_count = max(max_count, count)
+        # return max_count                
             
+# 3rd attempt optmized 13-08-2025 Passed
+
+        # count = 0
+        # max_count = 0
+        # seen = []
+        # i = 0
+
+        # while i < len(s):
+        #     if s[i] not in seen:
+        #         count+=1
+        #         seen.append(s[i])
+        #         i+=1
+        #     else:
+        #         max_count = max(max_count, count)
+        #         count = 0
+        #         seen = []
+        # max_count = max(max_count, count)
+
+        # return max_count
+
+# Revisit revisitted in 01 - 09 - 2025 ---------------------
+        left = 0
+        right = 0
+        dick = ''
+        maxi = 0
+        while right < len(s):
+
+            if s[right] not in dick:
+                dick += s[right]
+                right+=1        
+            elif s[right] in dick:
+                while s[right] in dick:
+                    dick = dick[1:]
+                    left += 1
+            maxi = max(maxi, right - left)
+        
+        return maxi
