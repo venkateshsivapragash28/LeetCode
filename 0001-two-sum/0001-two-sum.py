dcc2f577a7f22a518335fcd2440002f968067692
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-09-01 14:19:57 +0530

    Time: 3 ms (59.04%), Space: 13.1 MB (77.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range (i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+        dick = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in dick:
+                return [dick[diff],i]
+            else:
+                dick[num] = i
+        
