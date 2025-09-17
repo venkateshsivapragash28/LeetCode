66908bae67758474e564019ba52e60396f4fe915
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-09-17 10:43:23 +0530

    Time: 3 ms (34.11%), Space: 19.6 MB (90.17%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,27 +1,61 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isValidBST(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: bool
-        """
-        inorder = []
-        def backtrack(root):
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return
+        
+        # def backtrack(root):
+        #     if not root:
+        #         return 
+
+        #     if root.left:
+        #         if root.left.val > root.val:
+        #             return False
+        #     if root.right:
+        #         if root.right.val < root.val:
+        #             return False
+
+        #     if backtrack(root.left) and backtracK(root.right):
+        #         return True
+        #     return False
+
+        # def backtrack(root):
+        #     if not root:
+        #         return
+            
+        #     if root.left:
+        #         if root.left.val >= root.val:
+        #             return False
+
+        #     if root.right:
+        #         if root.right.val <= root.val:
+        #             return False
+                
+        #     return True
+
+        #     return backtrack(root.left) and backtrack(root.right)
+
+
+        # return backtrack(root)
+        output = []
+        def inorder(root):
+            nonlocal output
             if not root:
                 return
-            
-            backtrack(root.left)
-            inorder.append(root.val)
-            backtrack(root.right)
 
-        backtrack(root)
+            inorder(root.left)
+            output.append(root.val)
+            inorder(root.right)
 
-        for i in range(len(inorder)-1):
-            if inorder[i] >= inorder[i+1]:
+        inorder(root)
+
+        for i in range(len(output)-1):
+            if output[i] >= output[i+1]:
                 return False
         return True
+
