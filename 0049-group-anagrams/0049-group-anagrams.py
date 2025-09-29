47ea7abcef234b27f916cd0fe6aa0e69cc429a57
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-09-29 20:11:00 +0530

    Time: 12 ms (60.52%), Space: 20.7 MB (77.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,25 +1,8 @@
-class Solution(object):
-    def groupAnagrams(self, words):
-        #31 - 07 - 2025
-        # seen = [False]*len(words)
-        # final = []
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        output = defaultdict(list)
+        for i in strs:
+            key = ''.join(sorted(i))
+            output[key].append(i)
 
-        # for i in range(len(words)):
-        #     temp = [words[i]]
-        #     if seen[i] == False:
-        #         seen[i] = True
-        #         for j in range(i+1, len(words)):
-        #             if sorted(words[i]) == sorted(words[j]):
-        #                 seen[j] = True
-        #                 temp.append(words[j])
-        #         final.append(temp)
-
-        # return final
-
-        dict = defaultdict(list)
-
-        for word in words:
-            key = tuple(sorted(word))
-            dict[key].append(word)
-
-        return list(dict.values())
+        return list(output.values())
