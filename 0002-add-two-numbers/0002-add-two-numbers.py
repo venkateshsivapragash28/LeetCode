6f626a7b15de98cff7a45125ee0a16420b7a222f
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-11-19 00:19:38 +0530

    Time: 8 ms (19.47%), Space: 17.9 MB (56.66%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,36 +1,31 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        digit = ''
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        str1 = ''
         current = l1
         while current:
-            digit = digit + str(current.val)
+            str1 += str(current.val)
             current = current.next
-
-        digit1 = ''
+        str1 = str1[::-1]
+        str2 = ''
         current = l2
         while current:
-            digit1 = digit1 + str(current.val)
+            str2 += str(current.val)
             current = current.next
-        digit = str(digit[::-1])
-        digit1 = str(digit1[::-1])
-        res = str(int(digit) + int(digit1))
-        res = res[::-1]
-        dummy = ListNode(0)
-        current = dummy
-        for i in str(res):
+        str2 = str2[::-1]
+        res = str(int(str1) + int(str2))[::-1]
+
+        if len(res) == 0:
+            return ListNode(0)
+        l3 = ListNode(int(res[0]))
+        res = res[1:]
+        current = l3
+        for i in res:
             current.next = ListNode(int(i))
             current = current.next
-        result = dummy.next
 
-        return result
-        
+        return l3
