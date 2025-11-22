99cbe06c0c5de3c032dc79f69a753dac3d4b97c7
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-11-22 18:50:05 +0530

    Time: 19 ms (13.79%), Space: 28.4 MB (10.84%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,20 +1,41 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
 
+        # i = 0
+        # j = 0
+        # kootal = 0
+        # mini_len = float('inf')
+        # while i < len(nums) and j < len(nums):
+        #     kootal = kootal + nums[j]
+
+        #     while kootal >= target:
+        #         mini_len = min(mini_len, j - i)
+        #         kootal = kootal - nums[i]
+        #         i += 1
+        #     j += 1
+
+        # if mini_len < float('inf'):
+        #     return mini_len + 1
+        # else:
+        #     return 0
+
+
         i = 0
         j = 0
-        kootal = 0
-        mini_len = float('inf')
+        summ = 0
+        minimum = float('inf')
         while i < len(nums) and j < len(nums):
-            kootal = kootal + nums[j]
+            summ += nums[j]
 
-            while kootal >= target:
-                mini_len = min(mini_len, j - i)
-                kootal = kootal - nums[i]
+            while summ >= target:
+                minimum = min(minimum, j - i)
+
+                summ -= nums[i]
                 i += 1
             j += 1
 
-        if mini_len < float('inf'):
-            return mini_len + 1
+
+        if minimum < float('inf'):
+            return minimum + 1
         else:
             return 0
