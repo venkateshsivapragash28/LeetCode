934268d6825a864c9401f32d5faee62f1d4b520f
Venkatesh Sivapragash <150530635+venkateshsivapragash28@users.noreply.github.com>
2025-12-11 14:29:57 +0530

    Time: 82 ms (90.53%), Space: 27.5 MB (66.26%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,19 +1,13 @@
-class Solution(object):
-    def dailyTemperatures(self, arr):
-        """
-        :type temperatures: List[int]
-        :rtype: List[int]
-        """
-        new = [0]* len(arr)
-        stack = [[float('-inf'), -1]]
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        ans = [0] * len(temperatures)
+        stack = []
 
-        for i in range(len(arr)-1, -1, -1):
-            while stack and stack[-1][0] <= arr[i]:
-                stack.pop()
-            if stack and stack[-1][0] > arr[i]:
-                new[i] = abs(i - stack[-1][1])
-            else:
-                new[i] = 0
-            stack.append([arr[i], i])
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                top = stack.pop()
+                ans[top] = i - top
+            stack.append(i)
 
-        return new
+        return ans
